f1a1b47c91195fafed59b8fd9be4d3d79d07f917
Deybid Santiago Moreno Ruiz <ingdeybid.moreno@gmail.com>
2026-07-15 13:18:28 -0500

    mejorando entrega, 3er push

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const btnInc = document.getElementById("btnIncrementar");
 const btnDec = document.getElementById("btnDecrementar");
 let aux = 0;
+function actualizarColor() {
+    if (aux >= 10) {
+        contador.style.color = "#000000";
+    } else {
+        contador.style.color = "#ffffff";
+    }
+}
+
+
 btnInc.addEventListener("click", function(){
     aux+=1;
     document.getElementById("contador").textContent = aux;
+    actualizarColor();
 });
 btnDec.addEventListener("click", function(){
     aux-=1;
     document.getElementById("contador").textContent = aux;
+    actualizarColor();
 });
+
 
 
